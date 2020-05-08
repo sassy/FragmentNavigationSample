9cc62eab6ece6676e95edc8b7179a32c3ca8f13c
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-05-08 18:13:37 +0900

    TIL

---
+++ app/src/main/java/sassy/github/io/hellofragment/MainFragment.kt
@@ -0,0 +1,48 @@
+package sassy.github.io.hellofragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import sassy.github.io.hellofragment.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentMainBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_nextFragment)
+        }
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MainFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
